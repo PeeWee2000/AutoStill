cba0c835997a29c22198f28564c418f796d9e545
PeeWee2000 <RHarrison@themagnigroup.com>
2019-03-09 04:25:20 -0500

    Began cleaning the perpiphrials class due to the complexity currently involved in assiging pin numbers to new devices

--- AutoStillDotNet/Classes/Peripherals.cs
+++ AutoStillDotNet/Classes/Peripherals.cs
@@ -8,56 +8,33 @@ namespace AutoStillDotNet
 {
     class Periphrials
     {
+        ArduinoDriver.ArduinoDriver driver = new ArduinoDriver.ArduinoDriver(ArduinoModel.Mega2560, ArduinoCOMPort(), true);
+
         public ArduinoDriver.ArduinoDriver InitializeArduinoDriver()
         {
-
             //Declare the arduino itself
             var properties = new SystemProperties();
-            string ArduinoPort = "";
-
-
-            ManagementScope connectionScope = new ManagementScope();
-            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
-
-            try
-            {
-                foreach (ManagementObject item in searcher.Get())
-                {
-                    string desc = item["Description"].ToString();
-                    string deviceId = item["DeviceID"].ToString();
-
-                    if (desc.Contains("Arduino"))
-                    {
-                        ArduinoPort = deviceId;
-                        break;
-                    }
-                }
-            }
-            catch (ManagementException e)
-            {
-                /* Do Nothing */
-                //Put code here to select the arduino manually
-            }
-
-            var driver = new ArduinoDriver.ArduinoDriver(ArduinoModel.Mega2560, ArduinoPort, true);
 
             //Digial inputs
             driver.Send(new PinModeRequest(properties.FVEmptySwtich, PinMode.Input));
             driver.Send(new PinModeRequest(properties.FVCompleteSwitch, PinMode.Input));
-            driver.Send(new PinModeRequest(properties.StillFluidPump, PinMode.Input));
             driver.Send(new PinModeRequest(properties.StillLowSwitch, PinMode.Input));
             driver.Send(new PinModeRequest(properties.StillHighSwitch, PinMode.Input));
 
             //Digital outputs
-            driver.Send(new PinModeRequest(properties.StillFillValve, PinMode.Output));
+            InitializePin(properties.StillFillValve, PinMode.Output);
+            InitializePin(properties.StillFluidPump, PinMode.Output);
+            InitializePin(properties.StillElement, PinMode.Output);
+            InitializePin(properties.StillFillValve, PinMode.Output);
+            
 
-            //Set all the starting values to 0 since the stupid relay board is jank
-            driver.Send(new DigitalWriteRequest(properties.StillFillValve, DigitalValue.Low));
-            System.Threading.Thread.Sleep(3000);
+
+            //System.Threading.Thread.Sleep(3000);
             driver.Send(new PinModeRequest(properties.StillFluidPump, PinMode.Output));
 
             driver.Send(new DigitalWriteRequest(properties.StillFluidPump, DigitalValue.High));
+            driver.Send(new DigitalWriteRequest(properties.StillFillValve, DigitalValue.High));
+
 
 
 
@@ -93,5 +70,58 @@ namespace AutoStillDotNet
             //Send the initialized driver object back to whatever called this sub
             return driver;
         }
+        private void InitializePin(byte Pin, PinMode pinMode)
+        {
+            driver.Send(new PinModeRequest(Pin, pinMode));
+            if (pinMode == PinMode.Output)
+            {
+                driver.Send(new DigitalWriteRequest(Pin, DigitalValue.High));
+            }
+        }
+        private void InitializePin(ArduinoDriver.ArduinoDriver Driver, byte Pin, PinMode pinMode)
+        {
+            Driver.Send(new PinModeRequest(Pin, pinMode));
+            if (pinMode == PinMode.Output)
+            { 
+                Driver.Send(new DigitalWriteRequest(Pin, DigitalValue.High));
+            }
+        }
+        private void InitializePin(ArduinoDriver.ArduinoDriver Driver, byte Pin, PinMode pinMode, DigitalValue digitalValue)
+        {
+            Driver.Send(new PinModeRequest(Pin, pinMode));
+            Driver.Send(new DigitalWriteRequest(Pin, digitalValue));
+        }
+
+        private static string ArduinoCOMPort()
+        {
+
+            string ArduinoPort = "";
+
+
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery); //These 3 lines query device manager for all active serial connections
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get()) //This loop searches the results obtained above and attempts to identify the arduino
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        ArduinoPort = deviceId;
+                        break;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                //Put code here to select the arduino manually
+            }
+            return ArduinoPort;
+        }
+
     }
 }
